e6e8a37f524c374febc1013beb39c03d5c2efe96
ZSerg <zsergt3@gmail.com>
2016-10-11 18:39:48 +0300

    first test

--- iot_server/iot_storage/tests.py
+++ iot_server/iot_storage/tests.py
@@ -7,4 +7,14 @@ class DeviceTestCase(TestCase):
                               Type='Building',
                               description='Some Smart building',
                               device_id='abcdef0123456789',
-                              attributes=
+                              attributes=[{'key':'key1'}, {'value':'value1'},
+                                          {'key':'key2'}, {'value':'value2'}]
+                              )
+
+    def test_device_creation(self):
+        dev = Device.objects.get(name='SmartDevice')
+        self.assertEqual(dev.Type, 'Building')
+        self.assertEqual(dev.description, 'Some Smart Building')
+        self.assertEqual(dev.device_id, 'abcdef0123456789')
+        self.assertEqual(dev.attributes, 'value1')
+
