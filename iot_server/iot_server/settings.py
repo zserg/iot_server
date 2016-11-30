ec0eb1cd756cccf65042abd04c19c0acce0a5ee4
zserg <zsergt3@gmail.com>
2016-11-30 20:53:39 +0300

    set DEBUG=False

--- iot_server/iot_server/settings.py
+++ iot_server/iot_server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6mtq^439v&98+gzzekl8gobb0dcqzag^&@=&w+rk-f%_)0yrhh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
